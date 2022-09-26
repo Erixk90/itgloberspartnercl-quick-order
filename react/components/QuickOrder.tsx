65e4d48cd8312520468b272b193fce18876de932
Erixk90 <erickleonesreyes@gmail.com>
2022-09-26 07:45:20 -0500

    feat: add styles to the component

--- react/components/QuickOrder.tsx
+++ react/components/QuickOrder.tsx
@@ -2,8 +2,19 @@ import React, { useState, useEffect } from "react";
 import { useMutation, useLazyQuery } from "react-apollo";
 import  UPDATE_CART from '../graphql/updateCart.graphql'
 import GET_PRODUCT from '../graphql/getProductBySku.graphql'
+import styles from './styles.css'
 
-const QuickOrder = () => {
+const QuickOrder = (/*{ blockClass }: { blockClass: string }*/) => {
+
+  /*  const container__quickorder = generateBlockClass(styles.container, blockClass)
+    const container__quickorderChildren = generateBlockClass(styles.container__quickorderChildren, blockClass)
+    const title__quickorder = generateBlockClass(styles.title__quickorder, blockClass)
+    const form__quickorder = generateBlockClass(styles.form__quickorder, blockClass)
+    const container__quickorderForm = generateBlockClass(styles.container__quickorderForm, blockClass)
+    const form__quickorderLabel = generateBlockClass(styles.form__quickorderLabel, blockClass)
+    const form__quickorderInputText = generateBlockClass(styles.form__quickorderInputText, blockClass)
+    const input__quickorderButton = generateBlockClass(styles.input__quickorderButton, blockClass)*/
+
     const [inputText, setInputTex] = useState('')
     const [search, setSearch] = useState('')
 
@@ -60,16 +71,16 @@ const QuickOrder = () => {
     }
 
     return(
-        <div className="flex">
-            <div className="outline w-1280 pa3 m2">
-                <h2> Compra rápida en tu Carulla</h2>
-                <form onSubmit={searchProduct}>
-                    <div>
-                    <label htmlFor="sku">Ingresa el Numero de SKU </label>
+        <div className={styles.container__quickorder}>
+            <div className={styles.container__quickorderChildren}>
+                <h2 className={styles.title__quickorder}> Compra rápida en tu Carulla</h2>
+                <form onSubmit={searchProduct} className={styles.form__quickorder}>
+                    <div className={styles.container__quickorderForm}>
+                    <label className={styles.form__quickorderLabel} htmlFor="sku">Ingresa el Numero de SKU </label>
 
-                    <input id="sku" type="text" onChange={handleChange}></input>
+                    <input className={styles.form__quickorderInputText} id="sku" placeholder='N° SKU' type="text" onChange={handleChange}></input>
                     </div>
-                    <input type="submit" value="Añadir al Carrito "/>
+                    <input className={styles.form__quickorderButton} type="submit" value="Añadir al Carrito "/>
                 </form>
             </div>
         </div>
