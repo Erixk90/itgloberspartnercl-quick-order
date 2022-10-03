4d94b9cbe72bf66bc9289a8ab7ec375a04c5bfc6
Erixk90 <erickleonesreyes@gmail.com>
2022-10-02 23:53:20 -0500

    styles: change some components colors styles

--- react/components/QuickOrder.tsx
+++ react/components/QuickOrder.tsx
@@ -64,7 +64,7 @@ const QuickOrder = () => {
         <div className={styles.container__quickorder}>
             <div className={styles.container__quickorderChildren}>
                 <h2 className={styles.title__quickorder}> Compra rápida en tu Carulla</h2>
-                <form onSubmit={searchProduct} className={styles.form__quickorder}>
+                <form className={styles.form__quickorder} onSubmit={searchProduct} >
                     <div className={styles.container__quickorderForm}>
                     <label className={styles.form__quickorderLabel} htmlFor="sku">Ingresa el Numero de SKU </label>
 
